f07d2b618cffe7b48e911e70a1c873351f22cefc
Shamimreza82 <shamimrezaone@gmail.com>
2023-09-14 19:17:39 +0600

    added header section

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from './Component/Header/Header'
+
+function App() {
+
+  return (
+    <div className='text-center text-3xl font-bold lg:py-5 py-2 bg-[#F3F3F3]'>
+      <Header></Header>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Component/Card/Card.jsx
@@ -0,0 +1,9 @@
+const Card = () => {
+    return (
+        <div>
+            <h1>Course Registration</h1> 
+        </div>
+    );
+};
+
+export default Card;
